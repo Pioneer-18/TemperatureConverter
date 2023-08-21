02e535c988bd64a9a8856f32314abc274c0742ad
Athira Nair <139950461+Pioneer-18@users.noreply.github.com>
2023-08-21 22:10:49 +0530

    Add files via upload

---
+++ tempconverter.py
@@ -0,0 +1,9 @@
+print("Enter Temperature In Celsius:")
+C=float(input())
+# convertion formula                                                       
+F= C * 1.80 +32.00
+print("Temperature In Fahrenheit :",F)
+
+
+
+                                                                    # AN
